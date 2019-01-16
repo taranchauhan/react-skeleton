a55f3756210452b20a0586d2914cd38cb4c05ab5
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2019-01-16 17:01:51 +0000

    Add another route

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const App = () => (
 
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/another-page" component={Home} />
       </div>
     </Router>
   </Wrapper>
